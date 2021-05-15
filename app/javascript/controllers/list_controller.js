f54feb559c60487093b3f7e61c5008e9f15e1a9e
Ubuntu <ubuntu@ip-172-31-32-176.us-east-2.compute.internal>
2021-05-15 14:40:38 +0000

    use stimulus to render modal form

--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -2,11 +2,19 @@ import { Controller } from "stimulus"
 import StimulusReflex from "stimulus_reflex"
 
 export default class extends Controller {
-
+static targets = ["modal"]
 
 connect(){
 	StimulusReflex.register(this)
  }
-
+ 
+ 
+ addList(e){
+   let modalController = this.application.getControllerForElementAndIdentifier(
+      this.modalTarget,
+      "modal"
+     );
+     modalController.open();
+ }
  
 }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "4d3ff1583f1d47a9ade67b190bc72972.vfs.cloud9.us-east-2.amazonaws.com"
 end
