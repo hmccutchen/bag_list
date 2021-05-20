ff48b5d24a9d3caa4eba5bb4bc994f91075bfa8f
Ubuntu <ubuntu@ip-172-31-32-176.us-east-2.compute.internal>
2021-05-20 13:13:13 +0000

    refactor form modals

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,23 +4,25 @@ import StimulusReflex from "stimulus_reflex"
 export default class extends Controller {
 
 
-connect(){
-	StimulusReflex.register(this)
+ connect() {
+  StimulusReflex.register(this)
  }
- 
- 
-open(){
- document.body.classList.add("modal-open");
- this.element.setAttribute("style", "display: block;");
- this.element.classList.add("show");
-document.body.innerHTML += '<div class="modal-backdrop fade show"></div>';
-}
+
+
+ open() {
+  document.body.classList.add("modal-open");
+  this.element.classList.add("fade");
+  this.element.setAttribute("style", "display: block;");
+  this.element.classList.add("show");
+  document.body.innerHTML += '<div class="modal-backdrop show"></div>';
+ }
 
  close() {
-        document.body.classList.remove("modal-open");
-        this.element.removeAttribute("style");
-        this.element.classList.remove("show");
-        document.getElementsByClassName("modal-backdrop")[0].remove();
-    }
- 
+
+  document.body.classList.remove("modal-open");
+  this.element.removeAttribute("style");
+  this.element.classList.remove("show");
+  document.getElementsByClassName("modal-backdrop")[0].remove();
+ }
+
 }
