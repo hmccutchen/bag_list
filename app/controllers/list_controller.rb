01d2665b020f1c1db8a070a65d85ba05493ad6c9
hmccutchen <mccutchen.hassan@gmail.com>
2021-06-19 20:41:29 -0400

    add users to lists

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -3,21 +3,18 @@ class ListController < ApplicationController
 	def index
 
 		@lists = List.all
-		@list ||= List.new
+		@list ||= current_user.lists.new
 	
 	end
  
  def new
- 	@list = List.new
+ 	@list = current_user.lists.new
  end
  
  def edit
  end
   private
-  
-  def set_list_item
-		# @list_item ||= ListItem.find(params[:id])
-  end
+
   
   def set_list 
   	@list ||= List.find(params[:id])
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
 	has_many :list_items, -> { order(position: :asc)}, dependent: :destroy
+	belongs_to :user
 	
 	acts_as_list  top_of_list: 0 
 	default_scope -> { order(position: :asc) }
